a42db681ff5ec2f78c5a5ae78e1a45049243f443
spring-dev-deepak <65223428+spring-dev-deepak@users.noreply.github.com>
2020-10-11 20:12:20 +0530

    port_config

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const postRouter = require('./routers/posts')
 
 const app = express()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
